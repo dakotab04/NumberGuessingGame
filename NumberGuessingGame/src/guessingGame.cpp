80c3a253cea7f7ec279e46054bd826533b24b7b7
dakotab04 <dakotablanchard04@gmail.com>
2025-05-14 15:06:11 -0400

    Added defensive implementation. Users can no longer guess non-integers or integers outside 0-100 range.

--- NumberGuessingGame/src/guessingGame.cpp
+++ NumberGuessingGame/src/guessingGame.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "guessingGame.h"
+#include <typeinfo>
 
 void guessingGame(int randomNum)
 {
@@ -24,6 +25,18 @@ void guessingGame(int randomNum)
 		{
 			std::cout << "Congratulations, you guessed it!" << '\n' << '\n';
 			return;
+		}
+		if (guess < 0 || guess > 100) // If number is not in range
+		{
+			std::cout << "Please guess an integer between 0-100." << '\n' << '\n';
+			std::cin >> guess;
+		}
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout << "Invalid input. Please enter an integer between 0-100." << '\n' << '\n';
+			std::cin >> guess;
 		}
 	}
 }
